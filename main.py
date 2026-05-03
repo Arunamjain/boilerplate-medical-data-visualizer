bf382ef699ab4c506b32cf514601b591aa0e15f7
Arunamjain <166137622+Arunamjain@users.noreply.github.com>
2026-05-03 18:07:39 +0530

    Update main.py to integrate unittest framework
    Refactor to use unittest for testing and call visualization functions.

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# This entrypoint file to be used in development. Start by reading README.md
 import medical_data_visualizer
-from unittest import main
+import unittest
+import test_module
 
-# Test your function by calling it here
+unittest.main(module='test_module', exit=False, verbosity=2)
+
 medical_data_visualizer.draw_cat_plot()
 medical_data_visualizer.draw_heat_map()
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
